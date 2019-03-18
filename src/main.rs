7548278cf558c8db84f93d10b33eff45c9aa8074
ChatterMonkey <basu.maya@icloud.com>
2019-03-18 12:26:54 -0700

    Release Version

--- src/main.rs
+++ src/main.rs
@@ -23,11 +23,11 @@ use std::thread;
 
 
 fn main() {
-    let net_text = connect4code::file_manager::read_text(&String::from("/Users/mayabasu/CLionProject/ConnectFour/hexapod/arm1_GW1000000")).unwrap();
+    let net_text = connect4code::file_manager::read_text(&String::from("/Users/mayabasu/CLionProject/ConnectFour/final_final_poof/arm2_GW100000")).unwrap();
     let net = connect4code::file_manager::reconstitute(&net_text);
     let human = connect4code::Human::Human{points:0};
     let fakehuman = connect4code::FakeHuman::FakeHuman{};
-    connect4code::Board::play_connect_four(&human,&fakehuman,true);
+    connect4code::Board::play_connect_four(&human,&net,true);
     let args: Vec<String> = env::args().collect();
     let id_string = &args[1];
     let end_goal_string = &args[2];
