d9daf386ab203002e1b734879452ca4be88eb3ed
ChatterMonkey <basu.maya@icloud.com>
2019-04-02 13:13:27 -0700

    Release Version

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ use std::time::{Duration, Instant};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 use std::thread;
-use std::time::Duration;
+//use std::time::Duration;
 
 
 fn main() {
